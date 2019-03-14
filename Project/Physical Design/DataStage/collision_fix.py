b20a3666e33e4b8d731d39d888d86f40881fcbc6
Qasimtalkin <abulqasim110@gmail.com>
2019-03-14 17:30:11 -0400

    accident table complete

--- Project/Physical Design/DataStage/collision_fix.py
+++ Project/Physical Design/DataStage/collision_fix.py
@@ -8,30 +8,34 @@ from sqlalchemy.orm import sessionmaker
 DB = "group_16"
 DB_U = "data_admin_16"
 DB_P = "password"
-
-
 engine = ("postgresql+psycopg2://"+DB_U+":"+ DB_P + "@127.0.0.1:5432/"+ DB)
-#engine = ("postgresql+psycopg2://data_admin_16:password@127.0.0.1:5432/group_16")
 session = sessionmaker()
 session.configure(bind=engine)
 
 def main():
-#database engine.    
-
-
+    create_acc_df()
+    
 #Read the excel files
-    ac_data_frame = pandas.read_excel(os.path.abspath("h2017collisionsfinal.xlsx"), header = 0)
-    accident_tbl(ac_data_frame)
-
-#accident flat file construction.
-def accident_tbl(ac_data_frame):
+def create_acc_df():
+    ac_data_frame = pandas.read_csv(os.path.abspath("h2017collisionsfinal.csv"), header = 0, encoding = 'ISO-8859-1')
+    ac_16_df = pandas.read_csv(os.path.abspath("2016collisionsfinal.csv"), header = 0, encoding = 'ISO-8859-1')
+    ac_data_frame = ac_data_frame.append(ac_16_df)
+    ac_15_df = pandas.read_csv(os.path.abspath("2015collisionsfinal.csv"), header = 0, encoding = 'ISO-8859-1')
+    ac_data_frame = ac_data_frame.append(ac_15_df, ignore_index=True)
+    ac_14_df = pandas.read_csv(os.path.abspath("2014collisionsfinal.csv"), header = 0, encoding = 'ISO-8859-1')
+    ac_data_frame = ac_data_frame.append(ac_14_df, ignore_index=True)
+    print("16 : ", ac_16_df.shape)
+    print("Tot", ac_data_frame.shape)
+    insert_accident_tbl(ac_data_frame)
+    
+#accident flat file construction and adding data onto Db
+def insert_accident_tbl(ac_data_frame):
     accident_columns = ['accident_time', 'environment', 'road_surface', 'traffic_control', 'impact_type', 'visibility']
     accident_df = ac_data_frame[['Time', 'Environment', 'Road_Surface', 'Traffic_Control', 'Impact_type', 'Light' ]].copy()
     accident_df.columns = accident_columns
     accident_df['accident_key'] = range(0, len(accident_df))
-    accident_df.to_sql("accidents", engine, index=False, if_exists='append')
-    
-#adding data onto Db
-    
+    accident_df.to_sql("accidents", engine, index=False, if_exists='replace')
+  
+
 if __name__ == "__main__":
     main()
